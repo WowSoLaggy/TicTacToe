cb3a4bcc932714689a37d09ef23695be250de767
WowSoLaggy <dohxehapo@gmail.com>
2019-05-13 00:09:46 +0300

    (~) Added 'App.Dx' project

---
+++ App.Dx/App.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "App.h"
+
+
+void App::run()
+{
+}
---
+++ App.Dx/main.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+
+#include "App.h"
+
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+  App app;
+  app.run();
+
+  return 0;
+}
